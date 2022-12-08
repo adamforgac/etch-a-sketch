9b2652a5664c7862b014c765e5615fc0be602719
Adam Forgac <aforg@email.cz>
2022-12-08 19:04:02 +0100

    Create game grid
    Etch-a-sketch game grid was created using JS and CSS.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const container = document.querySelector(".grid-playground");
+
+for(let i = 0; i <= 15 ; i++) {
+    const cell = document.createElement("div");
+    container.appendChild(cell);
+    container.style["grid-template-columns"] = "repeat(4, 1fr)";
+    container.style["grid-template-rows"] = "repeat(4, 1fr)";
+}
+
 
