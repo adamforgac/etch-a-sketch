a8b69c337d5c9df57679a42a26298d89a2f6767a
Adam Forgac <aforg@email.cz>
2022-12-08 23:36:56 +0100

    Create function which resets the grid
    A function called resetGrid removes all divs from the container.
    This will be used in future for players to create their own grid sizes.

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
 const container = document.querySelector(".grid-playground");
+const slider = document.querySelector(".slider-move")
 
 
-for(let i = 0; i <= 15 ; i++) {
+// DEFAULT GAME GRID
+
+
+for(let i = 0; i <=  63; i++) {
     const cell = document.createElement("div");
     container.appendChild(cell);
-    container.style["grid-template-columns"] = "repeat(4, 1fr)";
-    container.style["grid-template-rows"] = "repeat(4, 1fr)";
+    container.style["grid-template-columns"] = "repeat(8, 1fr)";
+    container.style["grid-template-rows"] = "repeat(8, 1fr)";
 }
+
+
+// CHANGE THE GRID COLOR IF HOVERING
 
 
 const allCells = document.querySelectorAll(".grid-playground div");
 
+
 allCells.forEach(item => {
     item.addEventListener('mouseover', event => {
-      item.style.backgroundColor = "red"
+      item.style.backgroundColor = "black"
     })
-  })
+})
+
+
+// CLEAR THE GRID 
+
+
+function resetGrid() {
+    let allDivs = document.querySelectorAll(".grid-playground div")
+    allDivs.forEach(item => {
+        container.removeChild(item)
+    })
+}
+
+
+
+
+
